27cd12c73bf4604a2b2d38c65346fe22ce6ed586
hochstegerL03 <hochsteger.l03@htlwienwest.at>
2020-11-12 12:31:19 +0100

    Beta_1.0

--- js/v1.js
+++ js/v1.js
@@ -1,1 +1,12 @@
+function change(){
+    this.style.backgroundColor = 'yellow'
+}
 
+
+console.log("Test")
+let c = document.querySelectorAll('.card')
+console.log(c)
+
+for (const ttt of c) {
+    ttt.addEventListener('click', change)
+}
