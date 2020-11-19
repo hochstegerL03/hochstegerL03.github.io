929bb8333854ccdceb2f481986a5484c3b2ce0fc
hochstegerL03 <hochsteger.l03@htlwienwest.at>
2020-11-19 09:24:30 +0100

    Beta 1.0

--- js/v1.js
+++ js/v1.js
@@ -1,9 +1,33 @@
+var test =0
+var buffer = []
 function change(){
-    this.classList.toggle("flipped")
-    setTimeout(() => {  this.classList.toggle("flipped");}, 500);
+    if (test ==2) {
+        if (buffer[0].type == buffer[1].type) {
+            
+        }
+        for (let index = 0; index < buffer.length; index++) {
+            that = document.getElementById(buffer[index])
+            that.classList.toggle("flipped")
+        }
+        test =0
+        buffer = []
+    }
+    else{
+        this.classList.toggle("flipped")
+        test +=1 
+        buffer.push(this.id)
+    }
 }
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
-
+let moeglichkeiten = ["1","2","3","4","5","6","7","8"]
 console.log("Test")
 let xd = document.getElementById("card-deck")
 //let dxd = document.getElementsByClassName("deck")
@@ -13,11 +37,10 @@ for (let index = 0; index < 16; index++) {
     var div = document.createElement("div");
     div.classList.add("card")
     div.innerHTML = index+1;
+    div.id = index+1
+    div.addEventListener('click', change)
     xd.appendChild(div);
 }
-let c = document.querySelectorAll('.card')
-for (const ttt of c) {
-    ttt.addEventListener('click', change)
-}
+
 
 
