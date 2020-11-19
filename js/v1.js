17a6a26ee4f0b17da205f32a6d36c2f9658537d2
hochstegerL03 <hochsteger.l03@htlwienwest.at>
2020-11-19 08:31:17 +0100

    Beta 1.0

--- js/v1.js
+++ js/v1.js
@@ -1,6 +1,6 @@
 function change(){
-    this.style.backgroundColor = 'yellow'
-    setTimeout(() => {  this.style.backgroundColor = 'blue'; }, 500);
+    this.classList.toggle("flipped")
+    setTimeout(() => {  this.classList.toggle("flipped");}, 500);
 }
 
 
@@ -10,4 +10,12 @@ console.log(c)
 
 for (const ttt of c) {
     ttt.addEventListener('click', change)
+}
+let xd = document.getElementById("card-deck")
+for (let index = 0; index < 16; index++) {
+    
+    var div = document.createElement("div");
+    div.classList.add("card")
+    div.innerHTML = index+1;
+    xd.appendChild(div);
 }
