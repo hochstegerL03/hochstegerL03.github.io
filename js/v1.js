3c421212b776561591c0f4bfb7719af3feb85eda
hochstegerL03 <hochsteger.l03@htlwienwest.at>
2020-12-01 14:57:28 +0100

    Beta 3.0

--- js/v1.js
+++ js/v1.js
@@ -1,74 +1,118 @@
-var buffer = []
-var oeffen = []
-function change(){
-    if (buffer.length == 2) {
-        that = document.getElementById(buffer[0])
-        thaty = document.getElementById(buffer[1])
-        if (that.type == thaty.type) {
-            for (let index = 0; index <2; index++) {
-                document.getElementById(buffer[0]).classList.add("oeffen")
-               oeffen.push(buffer[index]) 
-                buffer.splice(index,1)
-            }
-    
-        }
-        for (let index = 0; index < buffer.length; index++) {
-            that = document.getElementById(buffer[index])
-            that.classList.toggle("zu")
-        }
+/*
+1) Leeres Array zum Speichern der aufgedeckten Karten (openedCards)
+2) Karten richtig erzeugen (type)[1,1,2,2,3,3,4,4.........8,8]
+3) Karten mischen (shuffle)
+4) Karten zum Deck hinzufügen
+5) Klick Eventhandler
+6) Kartenlogik:
+    6.1) prüfen ob karte bereits found + 
+    prüfen wie viele Karten bereits aufgedeckt sind? (0,1,2)
+    6.2) wenn weniger als 2 -> aufdecken
+    6.3) Erkennen ob zwei gleiche Karten 
+    aufgedeckt wurden -> wenn gleich -> found; wenn nicht gleich -> verdecken
+*/
+var superToggle = function(element, class0, class1) {
+    element.classList.toggle(class0);
+    element.classList.toggle(class1);
+  }
+var counter =0
+let reihe =0;
+let bonus =1;
+const numberCards = 16
+let openedCards = []
+let cardTypes=[]
+let farben = ["flipped1", "flipped2", "flipped3", "flipped4","flipped5","flipped6","flipped7","flipped8"]
+cardTypes = shuffle(numberCards)
+let p = document.querySelector('.deck')
+for (let index = 0; index < numberCards; index++) {
+        let c = document.createElement('div')
+        c.innerHTML = cardTypes[index]
+        c.type = cardTypes[index]
+        c.className="card"
+        c.classList.add("open") 
+        let ddd = farben[cardTypes[index]-1]
+        c.classList.add(ddd)
+        c.addEventListener('click',flipp)
+        p.appendChild(c)
+}
+
+function flipp(event){
+    //if(!this.classList.contains('found'))
+        openCard(this)
         
-        buffer = []
-    }
-    else if (!this.classList.contains("oeffen")){
-        this.classList.toggle("zu") 
-        if(buffer.length >0){
-            if (buffer[0] != this.id) {
-                buffer.push(this.id)
-                    
-            }
-            else{
-                buffer = []
-            }
+    
+}
+function openCard(c){
+    if (openedCards.length < 2 && !c.classList.contains("found")) {
+        if (!c.classList.contains('open')) {
+            openedCards.pop()
+            //c.classList.toggle("flipped")
+            superToggle(c,"flipped","open")
+            reihe -=2
         }
         else{
-            buffer.push(this.id)
+            c.classList.toggle('flipped')
+            c.classList.toggle("open")
+            openedCards.push(c)
         }
-                 
+        if (openedCards.length == 2) {
+            if(openedCards[0].type == openedCards[1].type){
+                //alert('gleich')
+                setTimeout(
+                    ()=>{
+                        for (let index = openedCards.length; index > 0; index--) {
+                            openedCards.pop().classList.toggle('found')
+                            
+                        }
+                        if (reihe <=2) {
+                            counter +=100* bonus
+                            bonus +=1
+                            reihe =0
+                        }
+                        else{
+                            counter +=100 
+                            reihe =0
+                            bonus =2
+                        }
+                        let county = document.getElementsByTagName("h1")
+                            county[0].innerHTML ="Points "+counter
+                    },
+                    1000
+                )
+            
+            }
+            else{
+                setTimeout(()=>{
+                    for (let index = openedCards.length; index >0; index--) {
+                        superToggle(openedCards.pop(),"flipped","open")
+                    } 
+                }, 1000)
+            }
+        }
+    }
         
+    reihe +=1
+    console.log(reihe)
+}
+function shuffle(laenge) {
+    let arra1 = []
+    for (let index = 0; index < laenge/2; index++) {
+        arra1.push(index+1)
+        arra1.push(index+1)
     }
-}
-let anzahl = []
-for (let index = 1; index < 9; index++) {
-    anzahl.push([index])
-    console.log(index)
-}
-
-let classen = ["flipped","flipped2", "flipped3", "flipped4", "flipped5", "flipped6", "flipped7", "flipped8"]
-console.log("Test")
-let xdxdxd = 1
-let xd = document.getElementById("card-deck")
-//let dxd = document.getElementsByClassName("deck")
-//let xdxd = dxd[0]
-for (let index = 0; index < 16; index++) {
-    var testy = Math.floor(Math.random() * anzahl.length)
-    while (anzahl[testy].length ==3) {
-        testy+=1
-        if (testy > anzahl.length-1) {
-            testy =0
-        }
+    var ctr = arra1.length, temp, index;
+// While there are elements in the array
+    while (ctr > 0) {
+// Pick a random index
+        index = Math.floor(Math.random() * ctr);
+// Decrease ctr by 1
+        ctr--;
+// And swap the last element with it
+        temp = arra1[ctr];
+        arra1[ctr] = arra1[index];
+        arra1[index] = temp;
     }
-    var div = document.createElement("div");
-    div.classList.add("card")
-    div.classList.add(classen[anzahl[testy][0]-1])
-    console.log(classen[anzahl[testy][0]])
-    div.classList.add("zu")
-    div.innerHTML = anzahl[testy][0];
-    div.id = index+1
-    div.type = anzahl[testy][0]
-    anzahl[testy].push("testy")
-    div.addEventListener('click', change)
-    xd.appendChild(div);
+    return arra1;
 }
-
 
 
